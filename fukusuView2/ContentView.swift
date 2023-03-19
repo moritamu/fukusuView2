d63f52d238f554df721ee1c7f69a583ea8b2a435
morisum <moris@r.sannet.ne.jp>
2023-03-19 17:21:26 +0900

    dekita1

--- fukusuView2/ContentView.swift
+++ fukusuView2/ContentView.swift
@@ -7,9 +7,17 @@
 
 import SwiftUI
 
+class ViewModel: ObservableObject {
+    @Published var name = ""
+}
+
 struct ContentView: View {
+    @ObservedObject var vm = ViewModel()
+    
     var body: some View {
         VStack {
+            SecondView(vm: vm)
+            Text("\(vm.name) second")
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
@@ -18,7 +26,23 @@ struct ContentView: View {
         .padding()
     }
 }
-
+struct SecondView: View {
+    @ObservedObject var vm: ViewModel
+    
+    var body: some View {
+        TextField("Placeholder", text: $vm.name)
+        ThirdView(vm: vm)
+    }
+}
+struct ThirdView: View {
+    @ObservedObject var vm: ViewModel
+    var body: some View {
+        VStack {
+            Text(vm.name)
+            Text("3rd")
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
