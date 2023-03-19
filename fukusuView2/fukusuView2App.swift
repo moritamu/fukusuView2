31f184a4d16442328d9c3acd67bd48e8dcaf83ee
morisum <moris@r.sannet.ne.jp>
2023-03-19 11:23:25 +0900

    Initial Commit

---
+++ fukusuView2/fukusuView2App.swift
@@ -0,0 +1,17 @@
+//
+//  fukusuView2App.swift
+//  fukusuView2
+//
+//  Created by MsMacM on 2023/03/19.
+//
+
+import SwiftUI
+
+@main
+struct fukusuView2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
